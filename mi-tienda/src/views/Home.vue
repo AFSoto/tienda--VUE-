ad99faae6d340438034de3ac8e3ea02785d683ca
Andres Felipe Soto Quintero <andresfelipesotoquintero@gmail.com>
2026-03-16 22:31:46 -0500

    ensayando rutas

---
+++ mi-tienda/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <p>esta es la pagina home </p>
+
+    <img src="" alt="">
+  </div>
+</template>
+
+<script setup>
+  const titulo = "bienvenido a la pagina de HOME"
+</script>
---
+++ mi-tienda/src/views/Shop.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{titulo}}</h1>
+        <p>Esta es mi Pagina Shop</p>
+        <img src="" alt="banner" class="banner">
+    </div>
+</template>
+
+<script setup>
+    const titulo="Bienvenido a Shop";
+</script>
+
+<style scoped>
+    p{
+        color: rgb(6, 54, 13);
+        font-size: 30px;
+    }
+
+    .banner{
+        width: 300px;
+        border-radius: 10px;
+    }
+</style>
